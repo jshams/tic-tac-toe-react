1f67bddd17cddd2a6d2a7b698467e748282a949e
Jake Shams <jshams62@gmail.com>
2019-04-02 12:24:39 -0700

    picking a key

--- src/Game.js
+++ src/Game.js
@@ -53,6 +53,16 @@ class Game extends Component {
         const history = this.state.history
         const current = history[history.length - 1]
         const winner = this.calculateWinner(current.squares)
+
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Go to move #' + move : 'Go to game start'
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        })
+
         let status
         if (winner) {
             status = 'Winner: ' + winner
@@ -71,7 +81,7 @@ class Game extends Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
