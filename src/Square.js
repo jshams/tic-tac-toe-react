461d01522b1b90a47101c834ca89e27213fb9be3
Jake Shams <jshams62@gmail.com>
2019-04-02 11:04:18 -0700

    X and O

--- src/Square.js
+++ src/Square.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react'
 
-class Square extends Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
-    //         value: null,
-    //     }
-    // }
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+// class Square extends Component {
+// constructor(props) {
+//     super(props)
+//     this.state = {
+//         value: null,
+//     }
+// }
+//     render() {
+//         return (
+//             <button
+//                 className="square"
+//                 onClick={() => this.props.onClick()}
+//             >
+//                 {this.props.value}
+//             </button>
+//         );
+//     }
+// }
+
+const Square = (props) => {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
 
 export default Square
